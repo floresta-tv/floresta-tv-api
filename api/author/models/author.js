4d376cf4ee0b8a04dcbbddd577e5aaadbe66fdca
vitorregisr <vitorregisrr@gmail.com>
2022-02-11 19:56:12 -0300

    Add Article, Author nd PageBlog API fields.

---
+++ api/author/models/author.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+const axios = require("axios");
+const vercelWebhook = strapi.config.get("custom.vercelWebhook");
+
+module.exports = {
+  lifecycles: {
+    async afterCreate(_, __) {
+      vercelWebhook && axios.post(vercelWebhook);
+    },
+    async afterUpdate(_, __, ___) {
+      vercelWebhook && axios.post(vercelWebhook);
+    },
+  },
+};
